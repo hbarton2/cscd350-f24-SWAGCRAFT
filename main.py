0c68beac9f26750a6ac19f25784b4b61f07e5d47
Robert Rutherford <79166091+ExoticPotato247@users.noreply.github.com>
2024-10-09 14:18:10 -0700

    Edit file name and add import statements to main

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 '''Main file'''
 
+# Import all other python files
+from classes.py import *
+from fields.py import *
+from interface.py import *
+from menu.py import *
+from methods.py import *
+from relationship.py import *
+from saveLoad.py import *
+
 def main():
-    pass
+    print("Hello, World!")
 
 if __name__=="__main__":
     main()
