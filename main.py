73f628fafbd94ddd00562a177bb0f928c72d65c3
Robert Rutherford <79166091+ExoticPotato247@users.noreply.github.com>
2024-10-09 14:37:11 -0700

    Add import statements to main

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 '''Main project file'''
 
+# Import all other python files
+from classes.py import *
+from fields.py import *
+from interface.py import *
+from menu.py import *
+from methods.py import *
+from relationship.py import *
+from saveLoad.py import *
+
 def main():
     print("hello, world")
 
