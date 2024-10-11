3ed13ecfa7d6a5f06adf81db77c1fcd81c98ddaa
afudge1 <166938653+afudge1@users.noreply.github.com>
2024-10-10 17:28:56 -0700

    SaveLoad.py
    Can now save and load the diagram, just need main to have the storage dict public.

--- saveLoad.py
+++ saveLoad.py
@@ -1,1 +1,23 @@
 '''Import and Export diagram'''
+
+#
+#   TODO: in main.py make the storage dictionary public
+#
+
+import json
+
+from main import *
+
+def save():
+    with open("data.json", "w") as file:
+        json.dump(diagram, file, indent=4)
+
+def load():
+    with open("data.json", "r") as file:
+        diagram = json.load(file)
+    
+def main():
+    save()
+
+if __name__ == "__main__":
+    main()
