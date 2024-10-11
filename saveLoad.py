3d8fa02c56fb375745cbd7b0d19187d85197224e
afudge1 <166938653+afudge1@users.noreply.github.com>
2024-10-10 17:35:09 -0700

    SaveLoad.py
    Fixed global variable issues

--- saveLoad.py
+++ saveLoad.py
@@ -1,18 +1,15 @@
 '''Import and Export diagram'''
 
-#
-#   TODO: in main.py make the storage dictionary public
-#
-
 import json
 
-from main import *
+from main import diagram
 
 def save():
     with open("data.json", "w") as file:
         json.dump(diagram, file, indent=4)
 
 def load():
+    global diagram
     with open("data.json", "r") as file:
         diagram = json.load(file)
     
