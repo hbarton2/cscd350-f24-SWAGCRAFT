832496a016b033535cf1ea2e544fb415d18f09e7
Thomas Longwell <Thomaslongwell360@gmail.com>
2024-10-14 14:52:07 -0700

    completing classes.py to classes branch

--- classes.py
+++ classes.py
@@ -1,1 +1,21 @@
 '''Define root class'''
+from main import diagram
+
+def addClass(name):
+    if name in diagram:
+        print(f"'{name}' already found in the diagram.")
+    else:
+        diagram.update({name: {}})
+
+def renameClass(oldName, newName):
+    if oldName in diagram:
+        diagram[newName] = diagram.pop(oldName)
+    else:
+        print(f"'{oldName}' not found in the diagram.")
+        
+def deleteClass(name):
+    if name in diagram:
+        del diagram[name]
+    else:
+        print(f"'{name}' not found in the diagram.")
+    
