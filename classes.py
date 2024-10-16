f21bf7983cb01a546acfd7ec76f56e36a98ad5d0
Thomas Longwell <Thomaslongwell360@gmail.com>
2024-10-15 18:26:46 -0700

    updated main to include a working menu as a temporary action an added notes to classes.py

--- classes.py
+++ classes.py
@@ -1,7 +1,7 @@
 '''Define root class'''
 
 #This imports the global dictionary from main
-from main import diagram
+from diagram import diagram
 
 def addClass(name):
     #checks if name is already in the diagram to avoid repeats
@@ -11,11 +11,13 @@ def addClass(name):
     #adds the class if it is not in the class already
     else:
         diagram.update({name: {}})
+        print(f"'{name}' added to the diagram.")
 
 def renameClass(oldName, newName):
     #checks if the old name is in the diagram then updates the name if it is
     if oldName in diagram:
         diagram[newName] = diagram.pop(oldName)
+        print(f"'{oldName} had been renamed to '{newName}")
     
     #if old name is not in the diagram it gives an error message
     else:
@@ -25,6 +27,7 @@ def deleteClass(name):
     #checks if name is in the diagram then deletes it if it is
     if name in diagram:
         del diagram[name]
+        print(f"'{name}' removed from the diagram.")
     
     #if name is not in the diagram it returns an error message
     else:
