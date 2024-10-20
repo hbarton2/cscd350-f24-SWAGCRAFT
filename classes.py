2a0c6f07ac851ea8d85f3e3741453549e062b7cc
afudge1 <166938653+afudge1@users.noreply.github.com>
2024-10-20 15:53:38 -0700

    classes.py unit tests

--- classes.py
+++ classes.py
@@ -1,15 +1,22 @@
 '''Define root class'''
-from main import diagram
+from diagram import diagram
 
 def addClass(name):
+    if name.strip() == "":
+        print("Class name cannot be empty or contain only whitespace.")
+        return
     if name in diagram:
         print(f"'{name}' already found in the diagram.")
     else:
         diagram.update({name: {}})
 
 def renameClass(oldName, newName):
+    if newName.strip() == "":
+        print("New class name cannot be empty or contain only whitespace.")
+        return
     if oldName in diagram:
-        diagram[newName] = diagram.pop(oldName)
+        if newName not in diagram:
+            diagram[newName] = diagram.pop(oldName)
     else:
         print(f"'{oldName}' not found in the diagram.")
         
