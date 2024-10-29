61814a087432b2594b3e9a7a423fc141b9ca756b
afudge1 <166938653+afudge1@users.noreply.github.com>
2024-10-28 20:29:43 -0700

    added comments

--- methods.py
+++ methods.py
@@ -3,6 +3,17 @@ from diagram import diagram
 from colorama import init, Fore, Style
 
 def addMethod(class_name, method_name, parameters):
+    """
+    Add a new method to a class in the diagram.
+
+    Parameters:
+    class_name (str): The name of the class to add the method to.
+    method_name (str): The name of the new method.
+    parameters (list): A list of parameter definitions in the format "param_name: param_type".
+
+    Returns:
+    bool: True if the method was added successfully, False otherwise.
+    """
     # Check if class_name is already in the diagram
     if class_name not in diagram:
         return False
@@ -28,6 +39,63 @@ def addMethod(class_name, method_name, parameters):
 
 
 def renameMethod(class_name, old_method_name, new_method_name, overload_index=None):
+    """
+This module provides functions for managing methods in a class-based diagram.
+"""
+
+from classes import *
+from diagram import diagram
+from colorama import init, Fore, Style
+
+def add_method(class_name, method_name, parameters):
+    """
+    Add a new method to a class in the diagram.
+
+    Parameters:
+    class_name (str): The name of the class to add the method to.
+    method_name (str): The name of the new method.
+    parameters (list): A list of parameter definitions in the format "param_name: param_type".
+
+    Returns:
+    bool: True if the method was added successfully, False otherwise.
+    """
+    # Check if class_name is already in the diagram
+    if class_name not in diagram:
+        return False
+    
+    # Get the class info and create 'Methods' if not found
+    class_info = diagram[class_name]
+    if 'Methods' not in class_info:
+        class_info['Methods'] = {}
+
+    # Get the Methods
+    methods = class_info['Methods']
+    
+    # Check if method is overloaded
+    if method_name not in methods:
+        methods[method_name] = [parameters]
+        return True
+    else:
+        if parameters in methods[method_name]:
+            return False
+        else:
+            methods[method_name].append(parameters)
+            return True
+
+
+def rename_method(class_name, old_method_name, new_method_name, overload_index=None):
+    """
+    Rename a method in a class in the diagram.
+
+    Parameters:
+    class_name (str): The name of the class that contains the method.
+    old_method_name (str): The current name of the method.
+    new_method_name (str): The new name for the method.
+    overload_index (int, optional): The index of the overloaded method to rename (default is None, which renames all overloads).
+
+    Returns:
+    bool: True if the method was renamed successfully, False otherwise.
+    """
     # Check if class_name is not in diagram
     if class_name not in diagram:
         return False
@@ -69,6 +137,17 @@ def renameMethod(class_name, old_method_name, new_method_name, overload_index=No
 
 
 def removeMethod(class_name, method_name, overload_index=None):
+    """
+    Remove a method from a class in the diagram.
+
+    Parameters:
+    class_name (str): The name of the class that contains the method.
+    method_name (str): The name of the method to remove.
+    overload_index (int, optional): The index of the overloaded method to remove (default is None, which removes all overloads).
+
+    Returns:
+    bool: True if the method was removed successfully, False otherwise.
+    """
     # Check if class_name is not in diagram
     if class_name not in diagram:
         return False
@@ -96,6 +175,19 @@ def removeMethod(class_name, method_name, overload_index=None):
 
 
 def addParameter(class_name, method_name, new_param_name, new_param_type, overload_index=None):
+    """
+    Add a new parameter to a method in a class in the diagram.
+
+    Parameters:
+    class_name (str): The name of the class that contains the method.
+    method_name (str): The name of the method to add the parameter to.
+    new_param_name (str): The name of the new parameter.
+    new_param_type (str): The type of the new parameter.
+    overload_index (int, optional): The index of the overloaded method to add the parameter to (default is None, which adds the parameter to all overloads).
+
+    Returns:
+    bool: True if the parameter was added successfully, False otherwise.
+    """
     # Check if class_name not in diagram
     if class_name not in diagram:
         return False
@@ -127,6 +219,18 @@ def addParameter(class_name, method_name, new_param_name, new_param_type, overlo
 
 
 def removeParameter(class_name, method_name, param_name, overload_index=None):
+    """
+    Remove a parameter from a method in a class in the diagram.
+
+    Parameters:
+    class_name (str): The name of the class that contains the method.
+    method_name (str): The name of the method to remove the parameter from.
+    param_name (str): The name of the parameter to remove.
+    overload_index (int, optional): The index of the overloaded method to remove the parameter from (default is None, which removes the parameter from all overloads).
+
+    Returns:
+    bool: True if the parameter was removed successfully, False otherwise.
+    """
     # Check if class_name not in diagram
     if class_name not in diagram:
         return False
@@ -160,6 +264,20 @@ def removeParameter(class_name, method_name, param_name, overload_index=None):
 
 
 def changeParameter(class_name, method_name, overload_index, param_index, new_name, new_type):
+    """
+    Change the name and type of a parameter in a method in a class in the diagram.
+
+    Parameters:
+    class_name (str): The name of the class that contains the method.
+    method_name (str): The name of the method to change the parameter in.
+    overload_index (int): The index of the overloaded method to change the parameter in.
+    param_index (int): The index of the parameter to change.
+    new_name (str): The new name for the parameter.
+    new_type (str): The new type for the parameter.
+
+    Returns:
+    bool: True if the parameter was changed successfully, False otherwise.
+    """
     if class_name not in diagram:
         return False
     
